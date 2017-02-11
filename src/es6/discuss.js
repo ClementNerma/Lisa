63d95cc30b5e982c2ce21c00abe8a14baa5465e4
Clément Nerma <clement.nerma@gmail.com>
2017-02-11 13:54:35 +0100

    User can input messages to talk with Lisa

--- src/es6/discuss.js
+++ src/es6/discuss.js
@@ -4,3 +4,26 @@ Lisa.dom.setAttribute('id', 'discuss');
 document.body.appendChild(Lisa.dom);
 
 // Here we can talk
+// Create an input field to allow user talking with Lisa
+let input = document.createElement('input');
+// Set it an ID
+input.setAttribute('id', 'input');
+// When something is typed in the input...
+input.addEventListener('keydown', e => {
+  // If the key pressed is 'Return' (Enter)...
+  if (e.keyCode === 13) {
+    // If there is no text in the input...
+    if (!input.value.length)
+      // Don't transmit it to Lisa
+      return ;
+
+    // Get the text
+    let request = input.value;
+    // Clear the input field
+    input.value = '';
+    // Display the request
+    Lisa.hears(request);
+  }
+});
+// Display the input field into the web page
+document.body.appendChild(input);
