0291475f183368bcec8f6f82cc84fab4d3fccfda
Clément Nerma <clement.nerma@gmail.com>
2017-02-14 19:56:03 +0100

    Sources files in the 'build' folder

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 :: NOTE: The Lisa's builder requires Node.js with the 'node-sass', 'babel' and
-::       'babili' packages
+::       'uglify-js' packages
 
 @echo off
 
@@ -17,19 +17,16 @@ if NOT "%1" == "" goto end
 echo [ build ] Fusionning all SCSS stylesheets...
 
 :: Reset the file
-echo. > build\lisa.fusion.scss
+echo. > build\lisa.scss
 
 :: For each source file (JavaScript), bundle it
 for %%f in (src\scss\*.scss) do (
-  type %%f >> build\lisa.fusion.scss
+  type %%f >> build\lisa.scss
 )
 
 :: Transpile to CSS
 echo [ build ] Transpiling to CSS...
-call node-sass build\lisa.fusion.scss --output-style=compressed > build\lisa.min.css
-
-:: Remove unneeded file
-del build\lisa.fusion.scss
+call node-sass build\lisa.scss --output-style=compressed > build\lisa.min.css
 
 :: End the build if the command-line arguments asked for it
 if NOT "%1" == "" goto end
@@ -39,25 +36,25 @@ if NOT "%1" == "" goto end
 echo [ build ] Fusionning all ES6 scripts...
 
 :: Enable strict mode in the bundle
-echo "use strict"; > build\lisa.fusion.js
+echo "use strict"; > build\lisa-es6.js
 :: Bundle every single file in the 'src\es6' folder
-type src\es6\index.js >> build\lisa.fusion.js
-type src\es6\lzstring.js >> build\lisa.fusion.js
-type src\es6\discuss.js >> build\lisa.fusion.js
-type src\es6\localdata.js >> build\lisa.fusion.js
-type src\es6\lis-parser.js >> build\lisa.fusion.js
+type src\es6\index.js >> build\lisa-es6.js
+type src\es6\lzstring.js >> build\lisa-es6.js
+type src\es6\discuss.js >> build\lisa-es6.js
+type src\es6\localdata.js >> build\lisa-es6.js
+type src\es6\lis-parser.js >> build\lisa-es6.js
 
 :: Babelify the ES6 bundle to make an ES5 one
 :: That extends the game's compatibility with older browsers, with no ES6 support
 cd build
 echo [ build ] Transpiling to ES5...
+:: Transpile the JavaScript ES6 bundle to ES5
+call babel lisa-es6.js --out-file lisa-es5.js --presets=es2015
+echo [ build ] Minifying...
 :: If minifying is not forbidden, do it
-if NOT "%2" == "--beautify" call babel lisa.fusion.js --out-file lisa.min.js --presets=es2015,babili
+if NOT "%2" == "--beautify" uglifyjs --compress --mangle --output lisa.min.js -- lisa-es5.js
 :: Else, don't minify the ES5 code
-if "%2" == "--beautify" call babel lisa.fusion.js --out-file lisa.min.js --presets=es2015
-
-:: Remove unneeded file
-del lisa.fusion.js
+if "%2" == "--beautify" copy lisa-es5.js lisa.min.js
 
 :: Return to the root directory
 cd ..
