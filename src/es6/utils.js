597f5e27c876ee640ed1da0c63a63200ea30fc36
Clément Nerma <clement.nerma@gmail.com>
2017-02-22 16:26:24 +0100

    Add date and time parsing utilities

--- src/es6/utils.js
+++ src/es6/utils.js
@@ -5,5 +5,39 @@
  * @type {Object}
  */
 Lisa.Utils = {
-  
+  /**
+   * Parse a date
+   * @param {string} input The date to parse
+   * @returns {Array.<number>} [dd,mm]
+   */
+  parseDate(input) {
+    return Lisa.getStandard(input, 'short_date').split('/').map(x => + x /* Fastest than parseInt() */);
+  },
+
+  /**
+   * Parse a long date
+   * @param {string} input The date to parse
+   * @returns {Array.<number>} [dd,mm,yyyy]
+   */
+  parseLongDate(input) {
+    return Lisa.getStandard(input, 'date').split('/').map(x => + x /* Fastest than parseInt() */);
+  },
+
+  /**
+   * Parse a time
+   * @param {string} input The time to parse
+   * @returns {Array.<number>} [hh,mm]
+   */
+  parseTime(input) {
+    return Lisa.getStandard(input, 'short_time').split('/').map(x => + x /* Fastest than parseInt() */);
+  },
+
+  /**
+   * Parse a long time
+   * @param {string} input The time to parse
+   * @returns {Array.<number>} [hh,mm,ss]
+   */
+  parseLongTime(input) {
+    return Lisa.getStandard(input, 'time').split('/').map(x => + x /* Fastest than parseInt() */);
+  }
 };
