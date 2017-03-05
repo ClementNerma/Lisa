920aaed27921f2af894cb37706e3de41209b0da7
Clément Nerma <clement.nerma@gmail.com>
2017-03-05 18:43:48 +0100

    Fix: Messages were not displayed when restoring Lisa's local state

--- src/es6/web-interface.js
+++ src/es6/web-interface.js
@@ -28,6 +28,31 @@ input.addEventListener('keydown', e => {
 // Display the input field into the web page
 document.body.appendChild(input);
 
+// Save the Lisa's state...
+// When a message is displayed
+Lisa.when('message', (date, author, message, className, allowHtml) => {
+  // Inject a DOM element
+  let dom = document.createElement('div');
+  // Set its attributes
+  dom.setAttribute('class', `message message-${className}`);
+  // Set the message, with the author's name
+  dom.innerHTML = `<strong>${author} : </strong>` + message;
+  // Append the element to the area
+  discuss.appendChild(dom);
+
+  // Scroll to the end of the container
+  // Get the amount of pixels until the scroll's maximum value
+  let remaining = discuss.scrollHeight - discuss.scrollTop;
+  // For a duration of 2000 ms (2 seconds), regurarily scroll near to the
+  // bottom of the discussion area
+  let interval = setInterval(() => discuss.scrollTop ++, Math.floor(2000 / remaining));
+  // After this delay, don't scroll anymore
+  setTimeout(() => clearInterval(interval), 2000);
+
+  // Save Lisa's state
+  localStorage.setItem('lisa_state', Lisa.State.convertObjectSave(Lisa.State.save()));
+});
+
 // Get the Lisa's state from the local storage
 let data = localStorage.getItem('lisa_state');
 
@@ -56,31 +81,6 @@ if (data)
     // Remove the corrupted data
     localStorage.removeItem('lisa_state');
   });
-
-// Save the Lisa's state...
-// When a message is displayed
-Lisa.when('message', (date, author, message, className, allowHtml) => {
-  // Inject a DOM element
-  let dom = document.createElement('div');
-  // Set its attributes
-  dom.setAttribute('class', `message message-${className}`);
-  // Set the message, with the author's name
-  dom.innerHTML = `<strong>${author} : </strong>` + message;
-  // Append the element to the area
-  discuss.appendChild(dom);
-
-  // Scroll to the end of the container
-  // Get the amount of pixels until the scroll's maximum value
-  let remaining = discuss.scrollHeight - discuss.scrollTop;
-  // For a duration of 2000 ms (2 seconds), regurarily scroll near to the
-  // bottom of the discussion area
-  let interval = setInterval(() => discuss.scrollTop ++, Math.floor(2000 / remaining));
-  // After this delay, don't scroll anymore
-  setTimeout(() => clearInterval(interval), 2000);
-
-  // Save Lisa's state
-  localStorage.setItem('lisa_state', Lisa.State.convertObjectSave(Lisa.State.save()));
-});
 
 // When Lisa answered to a request
 Lisa.when('did', () => localStorage.setItem('lisa_state', Lisa.State.convertObjectSave(Lisa.State.save())));
